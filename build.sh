1f5314b8f7abbbef1c920dcea1191f5399d43e6b
fine531 <d.phanindra0076@gmail.com>
2025-07-27 23:28:33 +0530

    Add Render deployment configuration files

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-# Build script for Render deployment
+set -o errexit
 
-set -o errexit  # exit on error
-
-pip install --upgrade pip
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 python manage.py migrate
